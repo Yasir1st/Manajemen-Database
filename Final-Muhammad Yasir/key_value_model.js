877a42a92eb3c2e315a63207ec7ae057ebd7b30e
Yasir1st <yasirnakano2nd@gmail.com>
2025-06-20 13:49:58 +0800

    Final Task

---
+++ Final-Muhammad Yasir/key_value_model.js
@@ -0,0 +1,8 @@
+// key_value_model.js
+db.kv.insertMany([
+  { key: "user:1", value: "Alice" },
+  { key: "user:2", value: "Bob" },
+  { key: "user:3", value: "Charlie" },
+  { key: "user:4", value: "Diana" },
+  { key: "user:5", value: "Evan" },
+]);
---
+++ Final-Muhammad Yasir/wide_column_model.js
@@ -0,0 +1,8 @@
+// wide_column_model.js
+db.students.insertMany([
+  { id: 1, name: "Alice", grades: { math: 85, science: 90 } },
+  { id: 2, name: "Bob", grades: { math: 78, science: 82 } },
+  { id: 3, name: "Charlie", grades: { math: 92, science: 88 } },
+  { id: 4, name: "Diana", grades: { math: 80, science: 89 } },
+  { id: 5, name: "Evan", grades: { math: 75, science: 85 } },
+]);
